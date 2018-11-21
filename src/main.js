4d876da0396b412ad08035be52b1c8de60ee4f62
danielpqe <danielpqe@gmail.com>
2018-11-21 15:01:17 -0500

    vue router

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import App from '@/App.vue'
+import VueRouter from 'vue-router'
 
+
+import routes from '@/routes'
 import EventBus from '@/plugins/event-bus'
-
+Vue.use(VueRouter)
 Vue.use(EventBus)
+
+const router = new VueRouter({routes})
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import Search from '@/components/Search.vue'
+
+const routes = [
+  {path: '/', components: Search, name: 'search'}
+]
+
+export default routes
